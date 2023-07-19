a155d152029d4f358e6a98738407eb4a7efab681
Juan Jimenez <jijimenez@uce.edu.ec>
2023-07-19 16:49:23 -0500

    Primer disenio

---
+++ app/src/main/java/com/jimenez/ecuafit/ui/activities/MenuActivity.kt
@@ -0,0 +1,60 @@
+package com.jimenez.ecuafit.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jimenez.ecuafit.R
+import com.jimenez.ecuafit.databinding.ActivityMenuBinding
+import com.jimenez.ecuafit.ui.fragments.DiarioFragment
+import com.jimenez.ecuafit.ui.fragments.InformeFragment
+import com.jimenez.ecuafit.ui.fragments.InicioFragment
+import com.jimenez.ecuafit.ui.utilities.FragmentsManager
+
+class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    override fun onStart() {
+        super.onStart()
+
+        initClass()
+    }
+
+    private fun initClass(){
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.inicio -> {
+                    // Respond to navigation item 1 click
+                    FragmentsManager().replaceFragment(
+                        supportFragmentManager,binding.frmContainer.id,InicioFragment()
+                    )
+                    true
+                }
+                R.id.diario -> {
+                    // Respond to navigation item 2 click
+                    FragmentsManager().replaceFragment(
+                        supportFragmentManager,binding.frmContainer.id,DiarioFragment()
+                    )
+
+                    true
+                }
+                R.id.informe -> {
+                    // Respond to navigation item 2 click
+                    FragmentsManager().replaceFragment(
+                        supportFragmentManager,binding.frmContainer.id,InformeFragment()
+                    )
+
+                    true
+                }
+                else -> false
+
+            }
+
+
+
+        }
+    }
+}
