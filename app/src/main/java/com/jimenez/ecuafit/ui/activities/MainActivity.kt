a81ba89110965fefbb30114b07c2aeb7a12e774a
Juan Jimenez <jijimenez@uce.edu.ec>
2023-07-20 07:27:26 -0500

    login2

--- app/src/main/java/com/jimenez/ecuafit/ui/activities/MainActivity.kt
+++ app/src/main/java/com/jimenez/ecuafit/ui/activities/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.jimenez.ecuafit.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.jimenez.ecuafit.R
+import com.jimenez.ecuafit.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-}
+    override fun onStart() {
+        super.onStart()
+        initClass()
+
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+    private fun initClass() {
+
+
+        binding.btnLogin.setOnClickListener {
+
+
+            //Intents
+            var intent = Intent(
+                this, MenuActivity::class.java
+            )
+
+            startActivity(intent)
+        }
+    }}
