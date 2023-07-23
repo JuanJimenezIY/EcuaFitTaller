bc36b814224f3304996b738e3f4acad4081842cf
Sebas0399 <antony.aguay@gmail.com>
2023-07-22 21:51:33 -0500

    login

--- app/src/main/java/com/jimenez/ecuafit/ui/activities/MainActivity.kt
+++ app/src/main/java/com/jimenez/ecuafit/ui/activities/MainActivity.kt
@@ -3,7 +3,11 @@ package com.jimenez.ecuafit.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.jimenez.ecuafit.R
 import com.jimenez.ecuafit.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
@@ -11,6 +15,7 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val db= FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -27,16 +32,32 @@ class MainActivity : AppCompatActivity() {
     }
     private fun initClass() {
 
+        title="Autenticacion"
 
         binding.btnLogin.setOnClickListener {
 
+            if(binding.txtName.text.isNotEmpty()&&binding.txtPassword.text.isNotEmpty()){
+                Log.d("UCE",binding.txtContraseA.text.toString())
+                Log.d("UCE",binding.txtCorreo.text.toString())
+                db.collection("users").document(binding.txtName.text.toString()).get().addOnSuccessListener {
+                    if(binding.txtPassword.text.toString()==it.get("contraseña")){
+                        //Intents
+                        var intent = Intent(
+                            this, MenuActivity::class.java
+                        )
 
-            //Intents
-            var intent = Intent(
-                this, MenuActivity::class.java
-            )
+                        startActivity(intent)
+                    }
+                    else{
+                        Snackbar.make(binding.txtContraseA,"Usuario o contraseña incorrectas",Snackbar.LENGTH_SHORT)
+                    }
+                }
+            }
+            else{
+                Snackbar.make(binding.txtContraseA,"Ingrese todos los campos",Snackbar.LENGTH_SHORT)
 
-            startActivity(intent)
+            }
+
         }
 
         binding.registro.setOnClickListener {
--- app/src/main/java/com/jimenez/ecuafit/ui/activities/RegistroActivity.kt
+++ app/src/main/java/com/jimenez/ecuafit/ui/activities/RegistroActivity.kt
@@ -2,11 +2,45 @@ package com.jimenez.ecuafit.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
 import com.jimenez.ecuafit.R
+import com.google.firebase.*
+import com.google.firebase.firestore.FirebaseFirestore
+import com.jimenez.ecuafit.databinding.ActivityRegistroBinding
 
 class RegistroActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityRegistroBinding
+    private val db=FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        binding=ActivityRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.buttonRegister.setOnClickListener {
+            Log.d("UCE","SAVE")
+            val res=db.collection("users").document(binding.editTextEmail.text.toString()).set(
+                hashMapOf("altura" to binding.editTextAltura.text.toString(),
+                "correo" to binding.editTextEmail.text.toString(),
+                "edad" to binding.editTextEdad.text.toString(),
+                "genero" to binding.editTextGenero.text.toString(),
+                "nombre" to binding.editTextName.text.toString(),
+                "peso" to binding.editTextPeso.text.toString(),
+                "contraseña" to binding.editTextContraseA.text.toString())
+            )
+            res.addOnCompleteListener {
+                if (it.isSuccessful){
+                    Snackbar.make(binding.textViewEmailLabel,"Registro completo",Snackbar.LENGTH_SHORT).show()
+                }
+            }
+
+
+        }
+    }
+    fun register(){
+
     }
 }
