79a047529038fd1d3731579eebe4b755a8cae4a8
Sebas0399 <antony.aguay@gmail.com>
2023-07-23 18:29:50 -0500

    macroNut

--- app/src/main/java/com/jimenez/ecuafit/ui/activities/DetailsComidasItems.kt
+++ app/src/main/java/com/jimenez/ecuafit/ui/activities/DetailsComidasItems.kt
@@ -6,26 +6,42 @@ import android.widget.ArrayAdapter
 import com.jimenez.ecuafit.data.Comida
 import com.jimenez.ecuafit.databinding.ActivityDetailsComidasItemsBinding
 import com.squareup.picasso.Picasso
+import kotlin.streams.toList
 
 class DetailsComidasItems : AppCompatActivity() {
-    private lateinit var binding:ActivityDetailsComidasItemsBinding
+    private lateinit var binding: ActivityDetailsComidasItemsBinding
+    private val macroNut = listOf<String>("Carbohidratos ", "Grasas ", " Proteinas")
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityDetailsComidasItemsBinding.inflate(layoutInflater)
+        binding = ActivityDetailsComidasItemsBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
     override fun onStart() {
         super.onStart()
-        val item=intent.getParcelableExtra<Comida>("name")
-        if(item!=null){
+        val item = intent.getParcelableExtra<Comida>("name")
+        if (item != null) {
 
-            binding.macroList.adapter= ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,item.macronutrientes)
-            binding.microList.adapter= ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,item.micronutrientes)
+            binding.macroList.adapter = ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                item.macronutrientes.stream().map {
+
+                    count++
+                    macroNut[count-1] + it + " gr"
+                }
+
+                    .toList())
+            binding.microList.adapter = ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                item.micronutrientes
+            )
 
 
-            binding.nombreComida.text=item.nombre
-            binding.calorias.text=item.calorias.toString()
+            binding.nombreComida.text = item.nombre
+            binding.calorias.text = item.calorias.toString()
             Picasso.get().load(item.foto).into(binding.imagenComida)
         }
     }
